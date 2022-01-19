fb5f05c8a60ecf831ea73f770ad4e83812864b98
Md Harun Or rashid <horrashid007@gmail.com>
2022-01-19 23:43:37 +0600

    print string

---
+++ print_string.asm
@@ -0,0 +1,20 @@
+.MODEL SMALL
+
+.STACK 100H
+
+.DATA
+    NUMBER DB 'A'
+
+.CODE
+MAIN PROC 
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    MOV AH,2
+    MOV DL,NUMBER
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
